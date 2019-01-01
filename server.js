0ffd06fb2b4dcea495584e24c7c95c84b9d51dfe
Glitch (hello-express) <none>
2019-01-01 12:39:56 +0000

    👑🐑 Checkpoint ./package.json:2671540/5284 ./server.js:2671540/3682 ./views/index.pug:2671540/2402 ./public/client.js:2671540/1170

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
-// server.js
-// where your node app starts
+var express = require('express')
+var app = express()
+app.use(express.static('public'))
+app.set('view engine', 'pug')
+app.locals.pretty = true;
+app.set('json spaces', 2);
 
-// init project
-const express = require('express');
-const app = express();
+var fetch = require('request')
+var today = new Date().toISOString().slice(0,10)
+var data
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use(function (request, response, next) {
+  if (! data) {
+    fetch.get({
+      url: 'https://quotes.wsj.com/AU/XASX/BKW/financials/annual/balance-sheet',
+    }, (err, res, body) => {
+      if (err) {
+        console.log('Error:', err)
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode)
+      } else {
+        console.log('Data loaded for ' + today)
+        data = body
+        next()
+      }
+    })
+  } else {
+    next()
+  }
+})
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.get('/', function (request, response) {
+  response.render('index', { date: today, data: data})
+})
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.get('/json', function (request, response) {
+  response.json(data);
+})
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
