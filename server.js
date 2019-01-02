2c2fb3dc61de5a58524e418965af7f0ac03d7648
Glitch (dfcf) <none>
2019-01-02 02:02:55 +0000

    Rewound to commit c00cae6735a49204729e87e898c9d47c7a1da850 + 0 edits

--- server.js
+++ server.js
@@ -9,14 +9,12 @@ const cheerio = require('cheerio')
 var fetch = require('request')
 var data = {}
 var symbols = ['BKW','CSL']
-//var statements = ['balance-sheet','cash-flow']
 
 app.use(function (request, response, next) {
   //if (! data.length) {
   for (let symbol of symbols) {
-    //for (let statement of statements)
-      fetch.get({
-        url: 'https://quotes.wsj.com/AU/XASX/'+symbol+'/financials/annual/balance-sheet',
+    fetch.get({
+      url: 'https://quotes.wsj.com/AU/XASX/'+symbol+'/financials/annual/balance-sheet',
     }, (err, res, body) => {
       if (err) {
         console.log('Error:', err)
@@ -32,39 +30,12 @@ app.use(function (request, response, next) {
           default:
             var multiplier = 1
         }
-        var st = parseInt($('td:contains("ST Debt & Current Portion LT Debt")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
-        var lt = parseInt($('td:contains("Long-Term Debt")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
-        //var fcf = parseInt($('td:contains("Free Cash Flow")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
-        //data[symbol] = st+lt/fcf
-        //console.log(st,lt,fcf,data)
+        var st = parseInt($('td:contains("ST Debt & Current Portion LT Debt")').first().parent().children().eq(1).text())*multiplier || 0
+        var lt = parseInt($('td:contains("Long-Term Debt")').first().parent().children().eq(1).text())*multiplier || 0
+        data[symbol] = st+lt
+        console.log(data)
         next()
-      
-    
-
-      fetch.get{
-        url: 'https://quotes.wsj.com/AU/XASX/'+symbol+'/financials/annual/cash-flow',
-    }, (err, res, body) => {
-      if (err) {
-        console.log('Error:', err)
-      } else if (res.statusCode !== 200) {
-        console.log('Status:', res.statusCode)
-      } else {
-        console.log('Data loaded')
-        const $ = cheerio.load(body)
-        switch($('.fiscalYr').eq(1).text()) {
-          case "All values AUD Millions.":
-            var multiplier = 1000000
-            break
-          default:
-            var multiplier = 1
-        }
-        //var st = parseInt($('td:contains("ST Debt & Current Portion LT Debt")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
-        //var lt = parseInt($('td:contains("Long-Term Debt")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
-        var fcf = parseInt($('td:contains("Free Cash Flow")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
-        data[symbol] = st+lt/fcf
-        console.log(st,lt,fcf,data)
-        next()
-      }}
+      }
     })
   }
   //} else {
