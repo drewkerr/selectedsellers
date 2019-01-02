18323b8342ea33b1e0efdeb14ac5290ba47cbb57
Glitch (hello-express) <none>
2019-01-02 01:32:23 +0000

    🏰🚣 Checkpoint ./server.js:2671921/71

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ var statements = ['balance-sheet','cash-flow']
 
 app.use(function (request, response, next) {
   //if (! data.length) {
-  for (let symbol of symbols) {
-    for (let statement of statements)
+  for (let statement of statements) {
+    for (let symbol of symbols)
       fetch.get({
         url: 'https://quotes.wsj.com/AU/XASX/'+symbol+'/financials/annual/'+statement,
     }, (err, res, body) => {
@@ -34,9 +34,9 @@ app.use(function (request, response, next) {
         }
         var st = parseInt($('td:contains("ST Debt & Current Portion LT Debt")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
         var lt = parseInt($('td:contains("Long-Term Debt")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
-        var cf = parseInt($('td:contains("Free Cash Flow")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
-        data[symbol] = st,lt,(st+lt)/cf
-        console.log(st,lt,cf,data)
+        var fcf = parseInt($('td:contains("Free Cash Flow")').first().parent().children().eq(1).text().replace(/[^0-9]/g, ''))*multiplier || 0
+        data[symbol] = st+lt/fcf
+        console.log(st,lt,fcf,data)
         next()
       }
     })
