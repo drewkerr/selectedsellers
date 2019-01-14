9cf5b3046d77856a12908a3a22948a2f6f24f58d
Glitch (dfcf) <none>
2019-01-14 11:55:29 +0000

    🍑🏁 Checkpoint ./server.js:225725/1050:2671921/8 ./public/style.css:225725/38

--- server.js
+++ server.js
@@ -8,58 +8,56 @@ const cheerio = require('cheerio')
 
 var fetch = require('request-promise')
 var data = {}
-var symbols = ['BKW','CSL','TGR','SNZ']
+var symbols = ['BKW','CSL','TGR','SNZ','CCL']
 var identifiers = { 'balance-sheet': { st: 'ST Debt & Current Portion LT Debt', lt: 'Long-Term Debt' },
                     'cash-flow': { fcf: 'Free Cash Flow' } }
 
 app.use(function (request, response, next) {
-  var promises = []
-  for (let symbol of symbols) {
-    data[symbol] = {}
-    for (let page in identifiers) {
-      promises.push(fetch.get({
-        url: 'https://quotes.wsj.com/AU/XASX/'+symbol+'/financials/annual/'+page,
-      }, (err, res, body) => {
-        if (err) {
-          console.log('Error:', err)
-        } else if (res.statusCode !== 200) {
-          console.log('Status:', res.statusCode)
-        } else {
-          console.log('Loaded',symbol,page)
-          const $ = cheerio.load(body)
-          switch($('.fiscalYr').eq(1).text()) {
-            case "All values AUD Millions.":
-              var multiplier = 1000000
-              break
-            case "All values NZD Millions.":
-              var multiplier = 1000000
-              break
-            case "All values AUD Thousands.":
-              var multiplier = 1000
-              break
-            case "All values NZD Thousands.":
-              var multiplier = 1000
-              break
-            default:
-              var multiplier = 1
-          }
-
-          for (let id in identifiers[page]) {
-            var number = $('td:contains("'+identifiers[page][id]+'")').first().parent().children().eq(1).text()
-            data[symbol][id] = parseFloat(number.replace(/[(]/,'-').replace(/[,]/g,''))*multiplier || 0
-            console.log(symbol,identifiers[page][id],data[symbol][id],number,multiplier)
-          }
-        }
-      }))
-    }
-  }
-  Promise.all(promises).then(function() {
+  if (!data) {
+    var promises = []
     for (let symbol of symbols) {
-      data[symbol]['dfcf'] = ((data[symbol]['st']+data[symbol]['lt'])/data[symbol]['fcf']).toFixed(2);
+      data[symbol] = {}
+      for (let page in identifiers) {
+        promises.push(fetch.get({
+          url: 'https://quotes.wsj.com/AU/XASX/'+symbol+'/financials/annual/'+page,
+        }, (err, res, body) => {
+          if (err) {
+            console.log('Error:', err)
+          } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode)
+          } else {
+            console.log('Loaded',symbol,page)
+            const $ = cheerio.load(body)
+            switch($('.fiscalYr').eq(1).text().split(' ').pop()) {
+              case "Millions.":
+                var multiplier = 1000000
+                break
+              case "Thousands.":
+                var multiplier = 1000
+                break
+              default:
+                var multiplier = 1
+            }
+
+            for (let id in identifiers[page]) {
+              var number = $('td:contains("'+identifiers[page][id]+'")').first().parent().children().eq(1).text()
+              data[symbol][id] = parseFloat(number.replace(/[(]/,'-').replace(/[,]/g,''))*multiplier || 0
+              console.log(symbol,identifiers[page][id],data[symbol][id],number,multiplier)
+            }
+          }
+        }))
+      }
     }
-    identifiers['cash-flow']['dfcf'] = "Debt / Free Cash Flow"
+    Promise.all(promises).then(function() {
+      for (let symbol of symbols) {
+        data[symbol]['dfcf'] = ((data[symbol]['st']+data[symbol]['lt'])/data[symbol]['fcf']).toFixed(2);
+      }
+      identifiers['cash-flow']['dfcf'] = "Debt / Free Cash Flow"
+      next()
+    })
+  } else {
     next()
-  })
+  }
 })
 
 app.get('/', function (request, response) {
