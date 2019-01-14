8dbe5c080f2ba15ba08dc5bf6eacbb51e2b9930e
Glitch (dfcf) <none>
2019-01-14 09:11:24 +0000

    🍱💪 Checkpoint ./server.js:2671921/116

--- server.js
+++ server.js
@@ -6,27 +6,28 @@ app.locals.pretty = true;
 app.set('json spaces', 2)
 const cheerio = require('cheerio')
 
+
 var fetch = require('request')
 var data = {}
 var symbols = ['BKW','CSL']
 var pages = ['balance-sheet','cash-flow']
 var indicators = ['ST Debt & Current Portion LT Debt','Long-Term Debt', 'Free Cash Flow' ]
-var st = 0;
-var lt = 0;
-var fcf = 0;
+var st = null;
+var lt = null;
+var fcf = null;
 
 for (let symbol of symbols)
-  data[0][0] = symbol
+  data[0] = symbol
   data[0][1][0] = "Short-term Debt and Current Portion Long-term Debt"
-  data[0][1][1] = st
+  data[0][1][1] = "st"
   data[0][2][0] = "Long-term Debt"
-  data[0][2][1] = lt
-  data[0][2][0] = "Free Cash Flow"
-  data[0][3][1] = fcf
-  data[0][2][0] = "Debt / Free Cash Flow"
-  data[0][4][1] = (st + lt) / fcf
+  data[0][2][1] = "lt"
+  data[0][3][0] = "Free Cash Flow"
+  data[0][3][1] = "fcf"
+  data[0][4][0] = "Debt / Free Cash Flow"
+  data[0][4][1] = "(st + lt) / fcf"
 
-
+console.log(data)
 /*
 class Company {
   constructor(symbol, st, lt, fcf){
@@ -103,6 +104,8 @@ app.getData(function (symbol, page, indicator){
 })
 */
 
+/*
+
 app.use(function (request, response, next) {
   //if (! data.length) {
   for (let symbol of symbols) {
@@ -165,9 +168,9 @@ app.use2(function (request, response, next) {
   }
 })
 
+*/
 
-
-
+/*
 app.get('/', function (request, response) {
   response.render('index', {data: data})
 })
@@ -179,3 +182,5 @@ app.get('/json', function (request, response) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+*/
