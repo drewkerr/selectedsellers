03a5ec20b3f8ee55d2a49d2b978f2ef08141e95f
Glitch (dfcf) <none>
2020-01-28 13:23:58 +0000

    🌍🌆 Checkpoint ./server.js:225725/890 ./views/index.pug:225725/59

--- server.js
+++ server.js
@@ -5,35 +5,29 @@ app.set('view engine', 'pug')
 const $ = require('cheerio')
 var fetch = require('request-promise-native')
 
-app.get('/search', function (request, response) {
+app.get('/search', (request, response) => {
   var promises = []
-  fetch.get(request.query.url)
-    .then(body => {
-      $("a[href^='https://www.ebay.com.au/str/']", body).eq(4).each( (i, e) => {
-        promises.push(
-          fetch.get( $(e).attr('href') )
-            .then(body => {
-              return $("a[href^='http://www.ebay.com.au/usr/']", body).eq(0).attr('href').slice(25)
-            })
-            .catch(err => { return err })
-        )
-      })
+  fetch.get(request.query.url).then(body => {
+    $("a[href^='https://www.ebay.com.au/str/']", body).each( (i, e) => {
+      promises.push(
+        fetch.get( $(e).attr('href') ).then(body => {
+            return $("a[href^='http://www.ebay.com.au/usr/']", body).eq(0).attr('href').slice(27)
+        }).catch(err => { return err })
+      )
     })
-    .catch(err => { return err })
-  })
-  
-  Promise.all(promises).then(stores => {
-    console.log(stores)
-    var url = 'https://www.ebay.com.au/sch/ebayadvsearch?_fsradio=%26LH_SpecificSeller%3D1&_sop=12&_saslop=1&_sasl='
-    url += stores.join('%2C')
-    response.redirect(url)
-  })
+    Promise.all(promises).then(stores => {
+      console.log(stores)
+      var url = 'https://www.ebay.com.au/sch/ebayadvsearch?_fsradio=%26LH_SpecificSeller%3D1&_sop=12&_saslop=1&_sasl='
+      url += stores.join('%2C')
+      response.redirect(url)
+    })
+  }).catch(err => { return err })
 })
 
-app.get('/', function (request, response) {
+app.get('/', (request, response) => {
   response.render('index')
 })
 
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
