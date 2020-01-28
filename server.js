b824d2b6890896a930fd291cb01aa5f59da8769f
Glitch (dfcf) <none>
2020-01-28 10:41:57 +0000

    💁💒 Checkpoint ./server.js:225725/6299

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cheerio = require('cheerio')
 var fetch = require('request-promise-native')
 
 app.post('/search', function (request, response) {
-  var stores = []
+  var promises = []
   fetch.get({ url: request.query.url }, (err, res, body) => {
     if (err) {
       console.log('Error:', err)
@@ -16,58 +16,21 @@ app.post('/search', function (request, response) {
     } else {
       const $ = cheerio.load(body)
       $("a[href^='https://www.ebay.com.au/str/']").each( (i, e) => {
-        
-      })
-    }
-  })
-  var promises = []
-  for (let store of stores) {
-    // only fetch if not already in data
-    if (!(symbol in data)) {
-      data[symbol] = {}
-      for (let page in identifiers) {
-        // fetch data from web page
         promises.push(fetch.get({
-          url: 'https://quotes.wsj.com/AU/XASX/'+symbol+'/financials/annual/'+page,
+          url: $(this).attr('href')
         }, (err, res, body) => {
           if (err) {
             console.log('Error:', err)
           } else if (res.statusCode !== 200) {
             console.log('Status:', res.statusCode)
           } else {
-            // load DOM parser (jQuery syntax)
-            const $ = cheerio.load(body)
-            // check symbol is valid
-            if ($('h1').eq(0).text() != 'Company Not Found') {
-              console.log('Loaded',symbol,page)
-              // check for multiplier note in page
-              switch ($('.fiscalYr').eq(1).text().split(' ').pop()) {
-                case "Millions.":
-                  var multiplier = 1000000
-                  break
-                case "Thousands.":
-                  var multiplier = 1000
-                  break
-                default:
-                  var multiplier = 1
-              }
-              for (let id in identifiers[page]) {
-                // extract data from page using DOM parser
-                var number = $('td:contains("'+identifiers[page][id]+'")').first().parent().children().eq(1).text()
-                // remove non-numeric characters, brackets to negatives, convert to number and multiply or zero if empty
-                data[symbol][id] = parseFloat(number.replace(/[(]/,'-').replace(/[,]/g,''))*multiplier || 0
-                console.log(symbol,identifiers[page][id],data[symbol][id],number,multiplier)
-              }
-            } else {
-              console.log('Invalid symbol',symbol)
-            }
+            return $("a[href^='https://www.ebay.com.au/usr/']").eq(0).attr('href').slice(26)
           }
         }))
-      }
-    } else {
-      next()
+      })
     }
-  }
+  })
+  
   // calculate DFCF once all data is retrieved
   Promise.all(promises).then(function() {
     for (let symbol of symbols) {
