ef0f3a37e7277d724b3a78ed1a0f4f5f4d511e5c
Glitch (dfcf) <none>
2020-01-28 11:49:58 +0000

    🍵🍴 Checkpoint ./server.js:225725/67

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ app.get('/search', function (request, response) {
         promises.push(
           fetch.get( $(e).attr('href') )
             .then( (body) => {
-              console.log(body)
-              return $("a[href^='https://www.ebay.com.au/usr/']", body).eq(0).attr('href').slice(26)
+              return $("a[href^='http://www.ebay.com.au/usr/']", body).eq(0).attr('href').slice(25)
             })
             .catch( (err) => {
               console.log(err)
@@ -32,6 +31,7 @@ app.get('/search', function (request, response) {
   })
   
   Promise.all(promises).then( (stores) => {
+    console.log(stores)
     var url = 'https://www.ebay.com.au/sch/ebayadvsearch?_fsradio=%26LH_SpecificSeller%3D1&_sop=12&_saslop=1&_sasl='
     url += stores.join('%2C')
     response.redirect(url)
